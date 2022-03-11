6a5d1ab3784f8444d426fdb7ea9a2b6ddc172a3a
kosukekashiwa <91003636+kosukekashiwa@users.noreply.github.com>
2022-03-11 15:49:18 +0900

    models tests

---
+++ src/features/article/models.test.ts
@@ -0,0 +1,21 @@
+import { normalizeArticles } from './models';
+
+describe('normalizr', () => {
+  it('normalizeArticles', () => {
+    const normalized = normalizeArticles([
+      { id: 1, title: 'title-1', author: { id: 1, name: 'name-1' } },
+      { id: 2, title: 'title-2', author: { id: 1, name: 'name-1' } },
+      { id: 3, title: 'title-3', author: { id: 2, name: 'name-2' } },
+    ]);
+
+    expect(normalized.result).toEqual([1, 2, 3]);
+    expect(normalized.entities).toEqual({
+      articles: {
+        1: { id: 1, title: 'title-1', author: 1 },
+        2: { id: 2, title: 'title-2', author: 1 },
+        3: { id: 3, title: 'title-3', author: 2 },
+      },
+      users: { 1: { id: 1, name: 'name-1' }, 2: { id: 2, name: 'name-2' } },
+    });
+  });
+});
